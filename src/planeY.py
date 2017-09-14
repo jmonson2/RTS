f4db06d3553725a345208773a433f8425e5e7ded
Johnathon Monson <johnathonmonson@gmail.com>
2017-09-14 15:57:22 -0400

    Removed past buffer changes. X and Z still need.

--- src/planeY.py
+++ src/planeY.py
@@ -4,11 +4,11 @@ class planeY:
         self.currentPos_Col = positionCol
 #Moves Row+=(Row+1) % 8 | Col+=2
     def move(self, positionMatrix):
-        positionMatrix[self.currentPos_Row][self.currentPos_Col] = 0
+        lastPos_Row = (self.currentPos_Row-1)%8
+        positionMatrix[lastPos_Row][self.currentPos_Col] = 0
         self.currentPos_Row = (self.currentPos_Row + 1) % 8
-
-
         positionMatrix[self.currentPos_Row][self.currentPos_Col] = 'Y'
+
         print("PLANE Y")
         print("Row:", self.currentPos_Row)
         print("Column:", self.currentPos_Col)
