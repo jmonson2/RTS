2dc0fedf0b45fc06f401b9f79f93299a8b520aed
Johnathon Monson <johnathonmonson@gmail.com>
2017-09-19 10:29:49 -0400

    Completed main function for bufferA bufferB

--- src/main.py
+++ src/main.py
@@ -21,35 +21,50 @@ class main:
     bufferB = [[0 for x in range(columns)] for y in range(rows)]
     bufferC = [[0 for x in range(3)] for y in range (3)]
     bufferD = [[0 for x in range(3)] for y in range (3)]
-
+    bufferC[0][0] = 'X'
+    bufferC[0][1] = 'Y'
+    bufferC[0][2] = 'Z'
+    bufferD[0][0] = 'X'
+    bufferD[0][1] = 'Y'
+    bufferD[0][2] = 'Z'
     print (bufferA)
 
     plane_X = planeX.planeX(0,0)
     plane_Y = planeY.planeY(0,2)
-    plane_Z = planeZ.planeZ(5,6)
+    plane_Z = planeZ.planeZ(3,6)
 
     
     for i in range(0,20):
         time.sleep(1)
         #Process 1 work
         if i % 2 == 0:
-           # plane_X.move(bufferA)
+            plane_X.move(bufferB)
             plane_Y.move(bufferB)
-           # plane_Z.move(bufferA)
+            plane_Z.move(bufferB)
 
             #if i > 1:
             #   bufferB = bufferA
         #Need P2 to write bufferC
         else:
-           # plane_X.move(bufferB)
+            plane_X.move(bufferA)
             plane_Y.move(bufferA)
-           # plane_Z.move(bufferB)
+            plane_Z.move(bufferA)
             #Need P2 to write bufferD
             #bufferA = bufferB
 
         print("BUFFER A", bufferA)
+        print("BUFFER B", bufferB)
+
+        #fillBufferC()
+
+#def fillBufferC(plane_X, plane_Y, plane_Z):
+    #Plane X
+    #bufferC[0][1] = plane_X.getRow()
+    #Plane Y
+    #Plane Z
+
+     
+    #Process 3 Work
     
-
-
 
 
