db2529cbd9b35dbb2476ca47911a40acb2f6099f
Johnathon Monson <johnathonmonson@gmail.com>
2017-09-19 10:30:53 -0400

    completed

--- src/planeZ.py
+++ src/planeZ.py
@@ -1,13 +1,50 @@
 class planeZ:
     def __init__(self, positionRow, positionCol):
-        self.currentPos_X = positionRow
+        self.currentPos_Row = positionRow
         self.currentPos_Col = positionCol
 #Moves Row+=3 | Col+=(Col+1) % 7
     def move(self, positionMatrix):
-        positionMatrix[self.currentPos_X][self.currentPos_Col] = 0
+        lastPos_Col = (self.currentPos_Col-1) % 7
+        self.subCheck(positionMatrix, self.currentPos_Row, lastPos_Col)
+        #positionMatrix[self.currentPos_Row][lastPos_Col] = 0
         
         self.currentPos_Col = (self.currentPos_Col + 1) % 7
-        positionMatrix[self.currentPos_X][self.currentPos_Col] = 'Z'
+        self.addCheck(positionMatrix)
+        #positionMatrix[self.currentPos_Row][self.currentPos_Col] = 'Z'
         print("PLANE Z")
-        print("Row:", self.currentPos_X)
+        print("Row:", self.currentPos_Row)
         print("Column:", self.currentPos_Col)
+
+    def getRow(self):
+        return self.currentPos_Row
+    def getCol(self):
+        return self.currentPos_Col
+
+
+    def addCheck(self, positionMatrix):
+        if positionMatrix[self.currentPos_Row][self.currentPos_Col] == 0:
+            positionMatrix[self.currentPos_Row][self.currentPos_Col] =  'Z'
+            return
+        elif positionMatrix[self.currentPos_Row][self.currentPos_Col] == 'X':
+            positionMatrix[self.currentPos_Row][self.currentPos_Col] = 'XZ'
+            return
+        elif positionMatrix[self.currentPos_Row][self.currentPos_Col] == 'Y':
+            positionMatrix[self.currentPos_Row][self.currentPos_Col] = 'YZ'
+            return
+        elif positionMatrix[self.currentPos_Row][self.currentPos_Col] == 'XY':
+            positionMatrix[self.currentPos_Row][self.currentPos_Col] = 'XYZ'
+            return
+
+    def subCheck(self, positionMatrix, lastPos_Row, lastPos_Col):
+        if positionMatrix[lastPos_Row][lastPos_Col] == 'Z':
+            positionMatrix[lastPos_Row][lastPos_Col] = 0
+            return
+        elif positionMatrix[lastPos_Row][lastPos_Col] == 'XZ':
+            positionMatrix[lastPos_Row][lastPos_Col] = 'X'
+            return
+        elif positionMatrix[lastPos_Row][lastPos_Col] == 'YZ':
+            positionMatrix[lastPos_Row][lastPos_Col] = 'Y'
+            return
+        elif positionMatrix[lastPos_Row][lastPos_Col] == 'XYZ':
+            positionMatrix[lastPos_Row][lastPos_Col] = 'XY'
+            return
